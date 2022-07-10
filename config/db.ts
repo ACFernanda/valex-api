27c7819be3ba304492fdbdaca7cee0dec93aa192
acfernanda <acfernanda98@gmail.com>
2022-07-10 14:55:58 -0300

    Prepare for deploy

--- config/db.ts
+++ config/db.ts
@@ -2,9 +2,16 @@ import pg from "pg";
 import "./setup.js";
 
 const { Pool } = pg;
-
-const db = new Pool({
+const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-});
+  ssl: {},
+};
 
+if (process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const db = new Pool(configDatabase);
 export default db;
